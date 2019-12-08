42f6956cf95c337bcc3cee3333df5f44e656893e
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-08 02:44:17 -0600

    updated index to use postRouter

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
-// const db = require("./data/db");
-
+const postRouter = require('./routers/postRouter')
 
 const server = express();
 server.use(express.json());
+server.use('/api/posts', postRouter);
 
 
 const port = 8080;
