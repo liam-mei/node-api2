aa18887759e87678a3cbae672d6a213ae4ae6dfb
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-08 02:43:23 -0600

    post post

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -3,4 +3,37 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+// post message from body
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  const post = { title, contents };
+
+  if (!title || !contents)
+    return res.status(400).json({
+      errorMessage: "Please provide both title and contents for the post"
+    });
+
+  db.insert(post)
+    .then(obj => {
+      db.findById(obj.id).then(post => {
+        res.status(201).json(post);
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "post should have saved but there was an error returning it",
+        err
+      });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "there was an error while saving the post", err });
+    });
+});
+
+
+
+router.put("/:id", (req, res) => {});
+
 module.exports = router;
