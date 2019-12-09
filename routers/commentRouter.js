a794c78006817b7fb5a90cf1960e1c470faf7154
Derrick Mei <derrick.mei.168@gmail.com>
2019-12-09 06:46:32 -0600

    post comment by postid

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const db = require("../data/db");
 
-const router = express.Router();
-
+const router = express.Router({
+  mergeParams: true
+});
 
 // post comment by postid
-router.post('/comments', (req, res) => {
+router.post("/comments", (req, res) => {
+  const { post_id, text } = req.body;
 
-})
+  if (!req.body.text)
+    return res.status(400).json({ error: "please provide text for comment" });
 
-// get all comments of post with id
-router.get('/comments', (req, res)=> {
+  db.insertComment({ post_id, text })
+    .then(obj => {
+      db.findCommentById(obj.id)
+        .then(comment => res.status(200).json(comment))
+        .catch(error =>
+          res.status(500).json({
+            error:
+              "comment should have saved but there was problem returning it"
+          })
+        );
+    })
+    .catch(() =>
+      res.status(404).json({ error: "post with that id not found" })
+    );
+});
 
-})
-
-// get comment with comment id
-router.get('/comments/:id', (req, res) => {
-
-})
 
 module.exports = router;
